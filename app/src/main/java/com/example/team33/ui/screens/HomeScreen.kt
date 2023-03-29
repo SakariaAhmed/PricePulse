9c330790bf79f3fd2e8147d292e8a40236f4a964
Sofia Ibn El Moujdi Troger <sofiai@ulrik.uio.no>
2023-03-29 13:06:19 +0200

    added "top" and "buttom" bar and nav

--- app/src/main/java/com/example/team33/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/team33/ui/screens/HomeScreen.kt
@@ -7,11 +7,33 @@ import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun HomeApp() {
+fun HomeApp(modifier: Modifier, onNavigateToNext: () -> Unit ) {
     val mainViewModel: MainViewModel = viewModel()
     val mainUiState by mainViewModel.uiState.collectAsState()
 
     HomeScreen(mainViewModel, mainUiState)
+    
+    Column(modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        //name of the screen
+        Text(text = "Home Screen")
+        
+        Column(modifier = Modifier.weight(3f)) {
+
+        }
+        //button to the next screen
+        Button(
+            onClick = onNavigateToNext,
+            Modifier
+                .height(75.dp)
+                .fillMaxWidth(), shape = RectangleShape
+        ) {
+            //name of the button
+            Text(text = "Prognose")
+        }
+    }
 }
 
 @Composable
