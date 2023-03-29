ea284fdaf0a6a8088d5a92ad80287737062cf75f
OnionHat <suleymanboyar02@gmail.com>
2023-03-29 14:07:11 +0200

    Fixed syntax error and added missing imports for navigation system

--- app/src/main/java/com/example/team33/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/team33/ui/screens/HomeScreen.kt
@@ -1,9 +1,18 @@
 package com.example.team33.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
