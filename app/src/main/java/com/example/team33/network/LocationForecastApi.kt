b88f73f8826134ee2c6ff76cb7eee78b34393a7e
AdamHuio <syedahu@ifi.uio.no>
2023-04-13 18:37:18 +0200

    Changed api call to proxy server

--- app/src/main/java/com/example/team33/network/LocationForecastApi.kt
+++ app/src/main/java/com/example/team33/network/LocationForecastApi.kt
@@ -6,7 +6,6 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
